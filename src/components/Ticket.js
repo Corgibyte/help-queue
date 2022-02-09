0eb993412a03795db88062b23029e527aa9f5d24
Hannah Young <hannah@corgibyte.com>
2022-02-09 15:20:32 -0800

    Update to end of edit/delete HW

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -3,18 +3,20 @@ import PropTypes from "prop-types";
 
 function Ticket(props) {
   return (
-    <React.Fragment>
+    <div onClick={() => props.whenTicketClicked(props.id)}>
       <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr />
-    </React.Fragment>
+    </div>
   );
 }
 
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  issue: PropTypes.string
+  issue: PropTypes.string,
+  id: PropTypes.string,
+  whenTicketClicked: PropTypes.func
 };
 
 export default Ticket;
